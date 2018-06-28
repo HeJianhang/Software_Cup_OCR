959b7d3d7147cc85767b6ae65ecfebf660f0e187
HeJianhang <a87821879@outlook.com>
2018-06-28 17:48:48 +0800

    commit

--- OCRDll/include/OCRDll.h
+++ OCRDll/include/OCRDll.h
@@ -14,11 +14,12 @@
 #include <yolo_v2_class.hpp>
 #include <string>
 #include <classifier.h>
+
 using namespace cv;
 using namespace cv::ml;
 using namespace std;
 using namespace classifier;
-
-__declspec(dllexport) void init();
+__declspec(dllexport) std::string& trim(std::string &s);
+__declspec(dllexport) void init(string path1,string path2,string path3,string path4);
 __declspec(dllexport) void destroy();
 __declspec(dllexport) void ocr(const string imgPath, string& name, string& num);
